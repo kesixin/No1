6c562ed10bced2c5ce5ef38d3893bfde9707186d
kesixin <462369233@qq.com>
2018-05-11 18:54:05 +0800

    用户信息

--- No1开始/app.js
+++ No1开始/app.js
@@ -4,37 +4,47 @@ Bmob.initialize("1d98cd6150536c8f790aa43e0b346ae3","a276fdd6e7a4192c93e9b23daa0f
 
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    //调用API从本地缓存中获取数据
+    try{
+      var value = wx.getStorageSync('openid');
+      if(value){
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+      }else{
+        wx.login({
+          success:function(res){
+            var user = new Bmob.User();//开始注册用户
+            user.loginWithWeapp(res.code).then(function(user){
+              var openid = user.get("authData").weapp.openid;
+              if(user.get("nickName")){
+                //第二次访问
+                wx.setStorageSync('openid', openid);
+              }else{
+                //保存用户信息
+                wx.getUserInfo({
+                  success:function(result){
+                    var userInfo= result.userInfo;
+                    var nickName = userInfo.nickName;
+                    var avatarUrl = userInfo.avatarUrl;
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+                    var u = Bmob.Object.extend("_User");
+                    var query = new Bmob.Query(u);
+                    query.get(user.id,{
+                      success:function(result){
+                        result.set('nickName',nickName);
+                        result.set('userPic',avatarUrl);
+                        result.set('openid',openid);
+                        result.save();
+                        wx.setStorageSync('openid', openid);
+                      }
+                    })
+                  }
+                })
               }
-            }
-          })
-        }
+            })
+          }
+        })  
       }
-    })
+    }catch(e){}
   },
   globalData: {
     userInfo: null
--- No1开始/pages/center/index.js
+++ No1开始/pages/center/index.js
@@ -1,66 +1,26 @@
 // pages/center/index.js
+var Bmob = require("../../utils/bmob.js");
+
 Page({
+  data:{},
+  onLoad:function(options){
+    var that = this;
+    var openid = wx.getStorageSync('openid');
+    if(openid){
+      var u = Bmob.Object.extend("_User");
+      var query = new Bmob.Query(u);
+      query.equalTo('openid',openid);
+      query.find({
+        success:function(results){
+          that.setData({
+            userInfo:results[0]
+          });
+        },
+        error:function(error){
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+        }
+      })
+    }
   }
+
 })
